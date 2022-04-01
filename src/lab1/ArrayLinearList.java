f1e0f5e17757316edcd57d66220d357aa9d9b0fc
zyma-se <74692674+zyma-se@users.noreply.github.com>
2022-04-01 14:49:03 +0800

    m

--- src/lab1/ArrayLinearList.java
+++ src/lab1/ArrayLinearList.java
@@ -1,49 +1,56 @@
 package lab1;
 
 public class ArrayLinearList {
-		public int size=0;
-		public ArrayLinearList() {
+	public int size = 0;
 
-		}
-		public boolean empty() {
-			if(size() == 0) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		public int size() {
-			return size;
-		}
-		public int getIndex(int[] a, int element) {
-			for(int i=0; i < size; i++) {
-				if(a[i] == element) {
-					return i;
-				}
-			}
-			return -1;
-		}
-		public int getElement(int[] a, int index) {
-			int element;
-			element = a[index];
-			return element;
-		}
-		public void add(int[] a, int c) {
-		    a[size()] = c;
-		    size=size + 1;
-		}
-		public void print(int[] a) {
-			String s = "";
-			System.out.print("Jagsaalt : ");
-			for(int i=0; i<this.size(); i++) {
-				System.out.print( this.getElement(a, i) + " " );
-			}
-		}
-		public void remove(int[] a, int index) {
-			for(int i=index; i<size-1; i++) {
-				a[i] = a[i+1];
-			}
-			size = size-1;
+	public ArrayLinearList() {
+
+	}
+
+	public boolean empty() {
+		if (size() == 0) {
+			return true;
+		} else {
+			return false;
 		}
 	}
+
+	public int size() {
+		return size;
+	}
+
+	public int getIndex(int[] array, int element) {
+		for (int i = 0; i < size; i++) {
+			if (array[i] == element) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	public int getElement(int[] array, int index) {
+		int element;
+		element = array[index];
+		return element;
+	}
+
+	public void add(int[] array, int c) {
+		array[size()] = c;
+		size = size + 1;
+	}
+
+	public void print(int[] array) {
+		String s = "";
+		System.out.print("Jagsaalt : ");
+		for (int i = 0; i < this.size(); i++) {
+			System.out.print(this.getElement(array, i) + " ");
+		}
+	}
+
+	public void remove(int[] array, int index) {
+		for (int i = index; i < size - 1; i++) {
+			array[i] = array[i + 1];
+		}
+		size = size - 1;
+	}
+}
