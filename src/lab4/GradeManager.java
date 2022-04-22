a1da92bbca77d11f8ba03e9f19d745fb9851261a
zyma-se <74692674+zyma-se@users.noreply.github.com>
2022-04-22 14:37:30 +0800

    heh

--- src/lab4/GradeManager.java
+++ src/lab4/GradeManager.java
@@ -34,6 +34,39 @@ public class GradeManager {
 	 * @param grade - grade to add to this grad manager
 	 */
 	public void addGrade(String grade) throws InvalidGradeException {
+//		int q, e = 0, r = 0, t = 0, y = 0, u = 0;
+//		if (Integer.parseInt(grade) <= 100 && Integer.parseInt(grade) >= 0) {
+//			q = Integer.parseInt(grade);
+//
+//			if (90 < q || q <= 100) {
+//				e = q;
+//			} else if (80 < q || q <= 90) {
+//				r = q;
+//			} else if (70 < q || q <= 80) {
+//				t = q;
+//			} else if (60 < q || q <= 70) {
+//				y = q;
+//			} else {
+//				u = q;
+//			}
+//		}
+//		else {
+//			grade = grade;
+//		}
+//		if (grade.equals("a") || grade.equals("A") || grade.equals(Integer.toString(e))) {
+//			allGrades.put(LetterGrade.A, this.allGrades.get(LetterGrade.A) + 1);
+//		} else if (grade.equals("b") || grade.equals("B") || grade.equals(Integer.toString(r))) {
+//			allGrades.put(LetterGrade.B, this.allGrades.get(LetterGrade.B) + 1);
+//		} else if (grade.equals("c") || grade.equals("C") || grade.equals(Integer.toString(t))) {
+//			allGrades.put(LetterGrade.C, this.allGrades.get(LetterGrade.C) + 1);
+//		} else if (grade.equals("d") || grade.equals("D") || grade.equals(Integer.toString(y))) {
+//			allGrades.put(LetterGrade.D, this.allGrades.get(LetterGrade.D) + 1);
+//		} else if (grade.equals("f") || grade.equals("F") || grade.equals(Integer.toString(u))) {
+//			allGrades.put(LetterGrade.F, this.allGrades.get(LetterGrade.F) + 1);
+//		} else {
+//			throw new InvalidGradeException("Buruu orolt bn" + " " + grade);
+//		}
+
 		if (grade.equals("a") || grade.equals("A")) {
 			allGrades.put(LetterGrade.A, this.allGrades.get(LetterGrade.A) + 1);
 		} else if (grade.equals("b") || grade.equals("B")) {
@@ -45,7 +78,7 @@ public class GradeManager {
 		} else if (grade.equals("f") || grade.equals("F")) {
 			allGrades.put(LetterGrade.F, this.allGrades.get(LetterGrade.F) + 1);
 		} else {
-			throw new InvalidGradeException("Buruu orolt bn" + grade);
+			throw new InvalidGradeException("Buruu orolt bn" + " " + grade);
 		}
 		// If grade doesn't match a valid grade, throw an InvalidGradeException
 	}
@@ -100,7 +133,6 @@ public class GradeManager {
 				if (input.startsWith("add")) {
 					String input2 = cin.readLine();
 					gm.addGrade(input2);
-
 				} else if (input.equals("print")) {
 					gm.printHistogram();
 				} else if (input.equals("exit")) {
